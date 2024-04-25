a4158719677dfdb8f63459bc804a20363b089d8f
designer-ab <per.useravi0@gmail.com>
2024-04-25 23:29:34 +0600

    add providers basic setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import './index.css'
-import routes from './Routes/Routes';
+import "./index.css";
+import routes from "./Routes/Routes";
+import AuthProviders from "./Providers/AuthProviders";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
-  </React.StrictMode>,
-)
+    <AuthProviders>
+      <RouterProvider router={routes} />
+    </AuthProviders>
+  </React.StrictMode>
+);
